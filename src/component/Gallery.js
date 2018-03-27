81550e11d733e98e3c53f1b8137206c24ed250d0
imnaifu <naifu_xu@outlook.com>
2018-03-27 22:46:08 +0800

    c

--- src/component/Gallery.js
+++ src/component/Gallery.js
@@ -2,41 +2,155 @@ import React, { Component } from 'react';
 import GalleryController from './GalleryController.js';
 import ImgFigure from './ImgFigure.js';
 
-require('../styles/main.scss');
+require('../styles/main.scss'); //这里是同步的
+const imgData = require('../data/images.json');
 
 class Gallery extends React.Component {
 
     constructor(){
-        super()
+        super();
+        this.imgFigureDoms = [];
         this.state = {
-            imgInfos: []
-        }
+            imgInfos: [],
+            imgFigures: [],
+            imgPosRange: {
+                horizontal: {
+                    left: {
+                        start: 0, //左侧水平方向最小值
+                        end: 0, //左侧水平方向最大值
+                    },
+                    right: {
+                        start: 0, //右侧水平方向最小值
+                        end: 0, //右侧水平方向最大值
+                    }
+                },
+                vertical: {
+                    start: 0, //垂直方向最小值
+                    end: 0, //垂直方向最大值
+                },
+                center: {
+                    left: 0, //中心图片位置
+                    top: 0, //中心图片位置
+                }
+            },
+            imgArrangeArr: []
+        };
+    }
+
+    componentWillMount(){
+        const imgInfos = this.addImgPath(imgData);
+        const imgFigures = this.getImgFigures(imgInfos);
+        // console.log(imgFigures);
+        // console.log(this.gallery);
+        this.setState({
+            imgInfos: imgInfos,
+            imgFigures: imgFigures,
+        });
     }
 
     componentDidMount(){
-        const imgData = require('../data/images.json');
+        const galleryWidth = this.gallery.scrollWidth;
+        const galleryHeight = this.gallery.scrollHeight;
+        const halfGalleryWidth = Math.ceil(galleryWidth / 2);
+        const halfGalleryHeight = Math.ceil(galleryHeight / 2);
+
+        const imgFigureWidth = this.imgFigureDoms_0.scrollWidth;
+        const imgFigureHeight = this.imgFigureDoms_0.scrollHeight;
+        const halfImgFigureWidth = Math.ceil(imgFigureWidth / 2);
+        const halfImgFigureHeight = Math.ceil(imgFigureHeight / 2);
+
+
         this.setState({
-            imgInfos: this.addImgPath(imgData)
-        })
+            imgPosRange: {
+                horizontal: {
+                    left: {
+                        start: -halfImgFigureWidth,
+                        end: halfGalleryWidth - halfImgFigureWidth*3,
+                    },
+                    right: {
+                        start: halfGalleryWidth + halfImgFigureWidth,
+                        end:  galleryWidth - halfImgFigureWidth,
+                    }
+                },
+                vertical: {
+                    start: -halfImgFigureHeight,
+                    end: galleryHeight - halfImgFigureWidth,
+                },
+                center: {
+                    left: halfGalleryWidth - halfImgFigureWidth, 
+                    top: halfGalleryHeight - halfImgFigureHeight,
+                }
+            },
+        });
+
+        this.rearrange(0);
     }
 
-    addImgPath(imgInfos){
-        imgInfos.forEach(function(each){
-            each['path'] = `../data/${each['name']}`;
+    /**
+     * [布局]
+     * @param  {[type]} centerIndex [description]
+     * @return {[type]}       [description]
+     */
+    rearrange(centerIndex){
+        const imgArrangeArr = this.state.imgArrangeArr;
+        const imgPosRange = this.state.imgPosRange;
+
+        const imgArrangeArrCenter = imgArrangeArr.splice(centerIndex, 1);
+        imgArrangeArrCenter.pos = imgPosRange.center;
+
+        imgArrangeArr.forEach( function(element, index) {
+            if (index%2 === 1){
+                //put to left side
+                
+            }else{
+                //put to right side
+            }
+        });
+        // const imgArrangeArrLeft = 
+    }
+
+    getRandomBetween(low, high){
+        return Math.ceil(Math.random() * (hight - low) + low);
+    }
+
+    addImgPath(imgData){
+        imgData.map(function(each){
+            each['path'] = require(`../data/${each['name']}`);
             return each;
         });
-        return imgInfos;
+        return imgData;
     }
 
-    renderImg(){
-    	return <ImgFigure />
-    }	
+    getImgFigures(imgInfos){
+        const imgFigures = [];
+        imgInfos.forEach((each, index) => {
+            if (!this.state.imgArrangeArr[index]){
+                const pos = {
+                    position: {
+                        left: 0,
+                        top: 0
+                    }
+                };
+                this.state.imgArrangeArr[index] = pos;
+                each['pos'] = pos; 
+            }else{
+                each['pos'] = this.state.imgArrangeArr[index]; 
+            }
+
+            imgFigures.push(<ImgFigure data={each} key={index} figureRef={(ref) => {
+                // this.imgFigureDoms.push(ref);
+                this[`imgFigureDoms_${index}`] = ref;
+            }}/>);            
+        }); 
+        return imgFigures;
+    }
 
     render() {
-        return (
-            <section className="gallery">
-                <section className="img-sec">
 
+        return (
+            <section className="gallery" ref={(ref) => {this.gallery=ref;}}>
+                <section className="img-sec">
+                    {this.state.imgFigures}
                 </section>
                 <nav className="gallery-controller">
                     <GalleryController />
--- src/component/ImgFigure.js
+++ src/component/ImgFigure.js
@@ -6,17 +6,23 @@ class ImgFigure extends Component {
 		super();
 	}
 
+	componentDidMount(){
+		// console.log(this.props);
+	}
+
 	// methods
 	render(){
 		return (
-			<figure>
-				<img/>
-				<figcaption>
-					<h2></h2>
+			<figure className='img-figure' ref={this.props.figureRef} style={this.props.data.pos}>
+				<img src={this.props.data.path}
+					alt={this.props.data.title}/>
+				<figcaption className='img-caption'>
+					<h2>{this.props.data.title}</h2>
 				</figcaption>
 			</figure>
 		);
 	}
+
 }
 
 export default ImgFigure;
